abeba134fa0f3a03abccc5f3cb99c2b501d527c0
Christian Jung <chris.j88@t-online.de>
2021-08-15 21:05:27 +0200

    Add JWT auth with some dump routes

--- src/Application.kt
+++ src/Application.kt
@@ -1,7 +1,11 @@
 package de.coli
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import com.google.gson.Gson
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
@@ -20,6 +24,13 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    val secret = environment.config.property("jwt.secret").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val myRealm = environment.config.property("jwt.realm").getString()
+
+
     install(Sessions) {
         cookie<MySession>("MY_SESSION") {
             cookie.extensions["SameSite"] = "lax"
@@ -34,6 +45,26 @@ fun Application.module(testing: Boolean = false) {
         gson {
         }
     }
+
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = myRealm
+            verifier(JWT
+                .require(Algorithm.HMAC256(secret))
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .build())
+
+            validate { credential ->
+                if (credential.payload.getClaim("username").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+        }
+    }
+
 
     routing {
         get("/") {
@@ -57,9 +88,8 @@ fun Application.module(testing: Boolean = false) {
         route("/user") {
             get {
                 val user = User(
-                    uuid = UUID.randomUUID().toString(),
-                    username = "Username",
-                    email = "E-Mail"
+                    username = "username",
+                    password = "password"
                 )
                 call.respond(Gson().toJson(user))
             }
@@ -67,12 +97,34 @@ fun Application.module(testing: Boolean = false) {
             // read the json from the body and put this into the dataclass
             post {
                 val user = call.receive<User>()
-                println(user)
+                val token = JWT.create()
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .withClaim("username", user.username)
+                    .withClaim("password", user.password)
+                    .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                    .sign(Algorithm.HMAC256(secret))
+                call.respond(hashMapOf("token" to token))
+            }
+        }
+
+        // need a valid token to get to this page
+        authenticate("auth-jwt") {
+            route("/hello") {
+                get{
+                    call.respondText("Hello world")
+                }
+                post {
+                    val principal = call.principal<JWTPrincipal>()
+                    val username = principal!!.payload.getClaim("username").asString()
+                    val user = principal!!.payload.getClaim("password").asString()
+                    call.respondText("Hello, $username! -> user : $user")
+                }
             }
         }
     }
 }
 
-data class User(val uuid: String, val username: String, val email: String)
+data class User(val username: String, val password: String)
 
 data class MySession(val count: Int = 0)
